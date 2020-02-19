5c533b5e403aee77a2b8baca8b0dfa565a89ebba
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-02-19 16:48:17 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
-version = '1.1.2'
+version = '2.0.dev'
 
 setup(
     name='plone.recipe.haproxy',
     version=version,
     description="Buildout recipe to install haproxy",
     long_description=(
-        open('README.txt').read() + '\n' +
+        open('README.rst').read() + '\n' +
         open('CONTRIBUTORS.txt').read() + '\n' +
         open('CHANGES.txt').read()),
     classifiers=[
         'Framework :: Buildout',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
     ],
     keywords='buildout haproxy proxy loadbalancer',
     author='Helge Tesdal',
